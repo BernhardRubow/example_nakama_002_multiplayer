eb0ae1b0851a23321bee89c0f0d9971bab55f614
Bernhard Rubow <brubow@newvisionsproject.de>
2018-08-08 15:29:39 +0200

    Working menu

--- Assets/_nvp/scripts/nvpGameManager.cs
+++ Assets/_nvp/scripts/nvpGameManager.cs
@@ -8,6 +8,8 @@ public class nvpGameManager : MonoBehaviour
 
     // +++ fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	public static Dictionary<int, string> SCENES;
+    public static string HOST;
+    public static int PORT;
 
 
 
@@ -29,6 +31,10 @@ public class nvpGameManager : MonoBehaviour
 	void InitGame(){
 		nvpGameManager.SCENES = new Dictionary<int, string>();
 		nvpGameManager.SCENES[0] = "menuMain";
+
+        nvpGameManager.HOST = "192.168.160.151";
+        nvpGameManager.PORT = 7350;
+
 
 		nvpEventManager.INSTANCE.InvokeEvent(GameEvents.OnGameInitialized, this, null);
 	}
---
+++ Assets/_nvp/scripts/nvpNetworkManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using newvisionsproject.managers.events;
+using Nakama;
+
+public class nvpNetworkManager : MonoBehaviour {
+
+	// +++ fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	private int _playerId;
+	private IClient _client;
+    private ISocket _socket;
+    private IChannel _channel;
+    private List<IUserPresence> _connectedUsers;
+    private ISession _session;
+    private string _id;
+    private string _userName;
+    private string _password;
+
+
+
+	// +++ unity callbacks ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	void Start () {
+		SubscribeToEvents();
+	}
+
+    void Destroy () {
+		UnsubscribeFromEvents();
+	}
+
+
+
+
+
+    // +++ event handler ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    private void LoginPlayer(object s, object e)
+    {
+        _playerId = (int)e;
+        this.LoadPlayerSettings();
+
+        this.StartNakameClient();
+    }
+
+
+
+
+    // +++ class methods ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private async void StartNakameClient(){
+        // Init List of connected users
+        _connectedUsers = new List<IUserPresence>(0);
+
+        // get the nakama client
+        _client = new Client("defaultkey", nvpGameManager.HOST, nvpGameManager.PORT, false);
+
+        // create a session
+        _session = await _client.AuthenticateEmailAsync(_userName, _password);
+
+        Debug.Log("Session created");
+    }
+
+    private void LoadPlayerSettings(){
+        if(_playerId == 1){
+            _userName = PlayerPrefs.GetString("Player1Email");
+            _password = PlayerPrefs.GetString("Player1Password");            
+        }
+        else {
+            _userName = PlayerPrefs.GetString("Player2Email");
+            _password = PlayerPrefs.GetString("Player2Password");            
+        }
+    }
+
+    private void SubscribeToEvents()
+    {
+        nvpEventManager.INSTANCE.SubscribeToEvent(GameEvents.OnLoginAsPlayerRequested, LoginPlayer);
+    }
+
+    private void UnsubscribeFromEvents()
+    {
+        nvpEventManager.INSTANCE.SubscribeToEvent(GameEvents.OnLoginAsPlayerRequested, LoginPlayer);
+    }
+}
+
